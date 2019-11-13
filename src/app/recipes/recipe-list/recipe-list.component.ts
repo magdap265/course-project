3a008772e1cd83d432bec3c54807079e7b5ef880
Magda <magdap.265@gmail.com>
2019-11-13 14:25:41 +0100

    Projekt ten tworzony jest na podstawie kursu 'Angular8' w ramach nauki framework'u Angular.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipie.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/reserve/EnF7DhHROS8OMEp2pCkx_Dufer%20food%20overhead%20hig%20res.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1057&q=80'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
