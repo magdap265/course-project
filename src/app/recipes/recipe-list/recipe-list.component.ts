57516643d0057159f030224221bfb5bc2780e3ab
Magda <magdap.265@gmail.com>
2019-11-22 12:15:42 +0100

    Dodanie funkcjonalności pozwalającej przełączć się między zakładkami oraz Wyświetlania szczegułu przepisu po kliknięciu w wybrany. Nauka komunikacji między komponentami (zastosowanie EventEmitter'a oraz dekortorów @Intup, @Output).

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipie.model';
 
@@ -8,13 +8,19 @@ import { Recipe } from '../recipie.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/reserve/EnF7DhHROS8OMEp2pCkx_Dufer%20food%20overhead%20hig%20res.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1057&q=80'),
+    new Recipe('Sernik z wiśniami', 'This is simply a test', 'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/sernik_z_wisniami.jpg')
   ];
+  // @Output() recipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
